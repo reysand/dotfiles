af4bf5eea27f02e6172fefd3f9c7af232ae90cb8
Andrey Slyusar <a97k572@gmail.com>
2020-09-30 15:33:47 +0300

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [[ $1 == "" || $2 == "" ]]; then
-	echo "usage: ./update.sh source_file target_file"
-	exit 0
-fi
-touch $2
-diff $2 $1 >> update.diff
-patch $2 update.diff
-rm update.diff
