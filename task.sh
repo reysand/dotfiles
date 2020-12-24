d629482b43c3313dc9b3e64aeb6c72e85fb5a1a2
Andrey Slyusar <a97k572@gmail.com>
2020-12-24 17:43:55 +0300

    Updated iterm profile

--- task.sh
+++ task.sh
@@ -67,7 +67,7 @@ case "$item" in
 		;;
 	i|I|iterm)
 		{ infocmp -1 xterm-256color ; echo "\tsitm=\\E[3m,\n\tritm=\\E[23m,"; } > /tmp/xterm-256color.terminfo
-tic /tmp/xterm-256color.terminfo
+		tic /tmp/xterm-256color.terminfo
 		echo "Italic enabled in iterm"
 		;;
 	c|C|clean)
